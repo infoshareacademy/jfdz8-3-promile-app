516d6a697768475945ab8317ee0d0386586277f9
Michal Muszynski <m.muszy@gmail.com>
2018-08-13 21:23:19 +0200

    Added map styles link

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'leaflet/dist/leaflet.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
