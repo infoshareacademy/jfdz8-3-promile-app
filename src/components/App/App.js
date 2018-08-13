1dbaa74dc015c7ce0aaf60696a0b8d293016c631
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-08-13 21:46:47 +0200

    add working marker on map

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from "../Map/Map";
-import L from "leaflet";
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 class App extends Component {
 
@@ -10,15 +11,21 @@ class App extends Component {
   };
 
   componentDidMount() {
-    const map = L.map('mapId').setView([54.35, 18.76], 15);
+    const map = L.map('mapId').setView([54.35, 18.76], 13);
+    let DefaultIcon = L.icon({
+      iconUrl: icon,
+      shadowUrl: iconShadow
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([54.35, 18.64]).addTo(map)
+    L.marker([54.35, 18.76]).addTo(map)
       .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
       .openPopup();
+
   }
 
   render() {
