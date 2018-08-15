6186f3b4ab4beafc6982027a934be2988381958c
Michal Muszynski <m.muszy@gmail.com>
2018-08-15 22:10:14 +0200

    Add user event marker on map with current events

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,20 +6,19 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 class MainMap extends Component {
   state = {
-    markers: [[54.35, 18.7], [54.37, 18.65]]
+    markers: [[54.405, 18.61], [54.40, 18.59], [54.40, 18.60], [54.41, 18.605]],
+    userMarkers: [[54.5, 18.6]]
   };
 
   clickHandler = (e) => {
     if (this.props.newMarker) {
-      this.state.markers.push([e.latlng.lat, e.latlng.lng]);
-      this.state.markers = this.state.markers.slice(-1);
-      this.setState({markers: this.state.markers})
+      this.state.userMarkers.push([e.latlng.lat, e.latlng.lng]);
+      this.state.userMarkers = this.state.userMarkers.slice(-1);
+      this.setState({userMarkers: this.state.userMarkers})
     } else {
       alert ('MAP IS BLOCKED')
     }
   };
-
-
 
   render() {
     const position = [54.40, 18.60];
@@ -43,6 +42,7 @@ class MainMap extends Component {
           />
           {this.state.markers.map((coordinates, id) =>
               <Marker key={`marker-${id}`} position={coordinates}>
+              <Marker key={`marker-${id}`} position={this.state.userMarkers[0]} />
             <Popup>
               <div className="popup">
               <span>Wanna add an Event?</span>
