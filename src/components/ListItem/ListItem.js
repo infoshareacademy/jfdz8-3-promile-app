3faa71614fd9527bf95858813f424a0657bd2e42
Michal Muszynski <m.muszy@gmail.com>
2018-09-04 19:38:49 +0200

    Prevent from multiple item click

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -23,7 +23,7 @@ class ListItem extends Component {
     this.setState({
       clicked: !this.state.clicked
     })
-  }
+  };
 
   render() {
     return(
@@ -32,7 +32,7 @@ class ListItem extends Component {
             {this.props.eventsList.map(event =>
             <li key={event.id}
                 className="single-event"
-                onClick={() => this.handleClickCallback(event.id)}
+                onClick={() => this.state.clicked ? false: this.handleClickCallback(event.id)}
             >
               <h2>{event.title}</h2>
               <p>{event.technology}</p>
