e04f0a389efa0a8740d883e0b7f4cf9c9f9cb910
Michal Muszynski <m.muszy@gmail.com>
2018-09-26 20:45:58 +0200

    Added require paths to icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -166,17 +166,17 @@ class App extends Component {
           />
           <div className="bottom_bar">
               <div className="bottom_bar-technology-logo-container">
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/angular_logo.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/c_plus_logo.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/database_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/game_dev_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/html_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/java_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/js_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/node_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/php_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/python_icon.png" /></span>
-                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/ruby_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/angular_logo.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/c_plus_logo.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/database_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/game_dev_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/html_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/java_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/js_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/node_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/php_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/python_icon.png")} /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src={require("../../images/tech_icons/ruby_icon.png")} /></span>
               </div>
           </div>
           <ToastContainer/>
