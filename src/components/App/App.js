9638a721c3a2747b4464e9c4553c1cb1b3926d9b
bart <decomplosed@gmail.com>
2018-09-26 11:33:57 +0200

    added spans with tech_icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -163,12 +163,19 @@ class App extends Component {
                clickedEvent={this.state.clickedEvent}
                user={this.state.user}
           />
-          <div className="bottom-bar">
-              <div className="add_event-container">
-                  {
-                      this.props.user &&
-                      <button className="add_event_button" onClick={this.showNewEventPanel}>Dodaj wydarzenie</button>
-                  }
+          <div className="bottom_bar">
+              <div className="bottom_bar-technology-logo-container">
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/angular_logo.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/c_plus_logo.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/database_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/game_dev_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/html_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/java_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/js_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/node_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/php_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/python_icon.png" /></span>
+                  <span className="bottom_bar-technology-logo-item"><img src="../../images/tech_icons/ruby_icon.png" /></span>
               </div>
           </div>
           <ToastContainer/>
