76badf7ce7b1210d29534dc8739ac26a95071643
bart <decomplosed@gmail.com>
2018-09-26 11:56:52 +0200

    added classnames to user buttons

--- src/components/ButtonsUserEvents/ButtonsUserEvents.js
+++ src/components/ButtonsUserEvents/ButtonsUserEvents.js
@@ -20,21 +20,24 @@ class ButtonsUserEvents extends Component {
 
   render() {
     return(
-      <div>
-        <button onClick={ () => this.props.userEvents ?
+      <div className="event_user_buttons-container">
+        <button className="event_user_button"
+                onClick={ () => this.props.userEvents ?
                                 this.getEvents() :
                                 this.getUserCreatedEvents() }
         >{this.props.userEvents ? 'Wszystkie wydarzenia' : 'Pokaż stworzone przeze mnie'}
         </button>
 
-        <button onClick={ () => this.props.userAttend ?
+        <button className="event_user_button"
+                onClick={ () => this.props.userAttend ?
                                 this.getEvents() :
                                 this.getEventsUserAttend() }
         >{this.props.userAttend ? 'Wszystkie wydarzenia' : 'Pokaż w których uczestniczę'}
         </button>
-          <button onClick={ () => this.props.userHasFavorites ?
-                                  this.getEvents() :
-                                  this.getUsersFavoriteEvents()}
+          <button className="event_user_button"
+                onClick={ () => this.props.userHasFavorites ?
+                                this.getEvents() :
+                                this.getUsersFavoriteEvents()}
           >
               {this.props.userHasFavorites ? 'Wszystkie wydarzenia' : 'Pokaż ulubione'}
           </button>
