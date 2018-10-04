53992e10ac9790d82b90440d3bcb0c11154c91a2
Bartosz <bartosh.zavada@gmail.com>
2018-10-04 22:20:17 +0200

    rebuild bottom bar component to increase maintenance ability

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -3,73 +3,33 @@ import React, { Component } from 'react'
 class BottomBar extends Component {
 
   handleIdOfClickedLogo = (technology) => {
+    this.props.getEvents()
     this.props.getClickedLogoTechnology(technology)
   }
 
-    getEvents = () => {
-      this.props.getEvents()
-    }
-
   render () {
+      const logotypes = [
+          {technology: 'Angular', logo: 'angular_logo.png'},
+          {technology: 'Cplus', logo: 'c_plus_logo.png'},
+          {technology: 'SQL', logo: 'database_icon.png'},
+          {technology: 'GameDev', logo: 'game_dev_icon.png'},
+          {technology: 'Html', logo: 'html_icon.png'},
+          {technology: 'Java', logo: 'java_icon.png'},
+          {technology: 'JavaScript', logo: 'js_icon.png'},
+          {technology: 'Node', logo: 'node_icon.png'},
+          {technology: 'PHP', logo: 'php_icon.png'},
+          {technology: 'Python', logo: 'python_icon.png'},
+          {technology: 'Ruby', logo: 'ruby_icon.png'}
+          ]
     return (
       <div className="bottom_bar">
-        <div className="bottom_bar-technology-logo-container">
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Angular")}>
-              <img alt="logo" src={require("../../images/tech_icons/angular_logo.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Cplus")}>
-              <img alt="logo" src={require("../../images/tech_icons/c_plus_logo.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("SQL")}>
-              <img alt="logo" src={require("../../images/tech_icons/database_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("GameDev")}>
-              <img alt="logo" src={require("../../images/tech_icons/game_dev_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Html")}>
-              <img alt="logo" src={require("../../images/tech_icons/html_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Java")}>
-              <img alt="logo" src={require("../../images/tech_icons/java_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("JavaScript")}>
-              <img alt="logo" src={require("../../images/tech_icons/js_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Node")}>
-              <img alt="logo" src={require("../../images/tech_icons/node_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("PHP")}>
-              <img alt="logo" src={require("../../images/tech_icons/php_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Python")}>
-              <img alt="logo" src={require("../../images/tech_icons/python_icon.png")}/>
-          </span>
-          <span className="bottom_bar-technology-logo-item" onClick={()=> this.props.logoClicked ?
-                                                                          this.getEvents() :
-                                                                          this.handleIdOfClickedLogo("Ruby")}>
-              <img alt="logo" src={require("../../images/tech_icons/ruby_icon.png")}/>
-          </span>
-        </div>
+          <div className="bottom_bar-technology-logo-container">
+              { logotypes.map( ({ technology, logo }) => (
+                  <span className="bottom_bar-technology-logo-item" onClick={()=> this.handleIdOfClickedLogo(technology)}>
+      <img alt="logo" src={require(`../../images/tech_icons/${logo}`)}/>
+    </span>
+              ))}
+          </div>
       </div>
     )
   }
